bd09b3d71e1e0d2e9027fed6a104e35b8af2c421
Muhammad Saqib Hussain <saqib9217@live.com>
2021-03-08 22:23:33 +0500

    implemented digit check

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,36 @@ use std::io;
 fn main() {
     let mut t = String::new();
     io::stdin().read_line(&mut t).expect("Error");
-    let testcases = 1;
+    let test_cases: u8 = t.trim().parse().expect("Error");
+    for _i in 1..(test_cases+1) {
+        //input
+        let mut inp = String::new();
+        io::stdin().read_line(&mut inp).expect("Error");
+        let split = inp.split_whitespace();
+        let vec = split.collect::<Vec<&str>>();
+        let lower = vec[0].parse::<u64>().unwrap();
+        let higher = vec[1].parse::<u64>().unwrap() + 1;
+
+        //logic
+
+        //all numbers
+        for i in lower..higher {
+            //each number
+            let mut num = i;
+            let mut vec = Vec::new();
+            while num != 0 {
+                vec.push(num/10);
+                num /= 10;
+            }
+            //check if digit is boring
+            for i in 0..(vec.len()) {
+                println!("Digit: {} and index: {}", vec[i], i);
+                if (i%2==0 && vec[i]%2!=0) || (i%2!=0 && vec[i]==0) {
+                    println!("boring");
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 }
