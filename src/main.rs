c7b30c379ddad4d08ca0c4fdf1499054a85d70d8
Muhammad Saqib Hussain <saqib9217@live.com>
2021-03-14 15:00:29 +0500

    trying

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 
 fn main() {
+    let mut is_boring = true;
+    let mut boring = 0;
     let mut t = String::new();
     io::stdin().read_line(&mut t).expect("Error");
     let test_cases: u8 = t.trim().parse().expect("Error");
@@ -13,26 +15,32 @@ fn main() {
         let lower = vec[0].parse::<u64>().unwrap();
         let higher = vec[1].parse::<u64>().unwrap() + 1;
 
-        //logic
+        //////////logic
 
-        //all numbers
         for i in lower..higher {
             //each number
-            let mut num = i;
+            //setup
             let mut vec = Vec::new();
+            let mut num = i;
+            //parsing int to number
             while num != 0 {
-                vec.push(num/10);
+                
+                vec.insert(0, num%10);
                 num /= 10;
             }
-            //check if digit is boring
-            for i in 0..(vec.len()) {
-                println!("Digit: {} and index: {}", vec[i], i);
-                if (i%2==0 && vec[i]%2!=0) || (i%2!=0 && vec[i]==0) {
-                    println!("boring");
+            // check if each is boring
+            for i in 0..vec.len(){
+                if (i%2==0 && vec[i]%2!=0) || (i%2!=0 && vec[i]%2==0) {
+                    is_boring = false;
                 } else {
-                    break;
+                    
                 }
             }
+            if is_boring==true {
+                boring+=1;
+                println!("{:?}", vec);
+            }
         }
+        println!("{}", boring);
     }
 }
