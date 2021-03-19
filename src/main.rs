bbbaa311dc6f167fe2b8b22868d03080ee1ce9b5
Muhammad Saqib Hussain <saqib9217@live.com>
2021-03-19 20:29:39 +0500

    partial google kickstart solution

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::io;
 
 fn main() {
-    let mut is_boring = true;
-    let mut boring = 0;
+    
     let mut t = String::new();
     io::stdin().read_line(&mut t).expect("Error");
     let test_cases: u8 = t.trim().parse().expect("Error");
-    for _i in 1..(test_cases+1) {
+    for i in 1..(test_cases+1) {
         //input
         let mut inp = String::new();
         io::stdin().read_line(&mut inp).expect("Error");
@@ -16,31 +15,39 @@ fn main() {
         let higher = vec[1].parse::<u64>().unwrap() + 1;
 
         //////////logic
+        let mut boring = 0;
+        let mut num_index = lower;
 
-        for i in lower..higher {
+        while num_index < higher {
             //each number
             //setup
             let mut vec = Vec::new();
             let mut num = i;
+            let mut is_boring = true;
             //parsing int to number
             while num != 0 {
                 
                 vec.insert(0, num%10);
                 num /= 10;
             }
+            
             // check if each is boring
             for i in 0..vec.len(){
+                //println!("Checking if {} is boring.", vec[i]);
                 if (i%2==0 && vec[i]%2!=0) || (i%2!=0 && vec[i]%2==0) {
-                    is_boring = false;
-                } else {
                     
+                } else {
+                    is_boring = false;
+                    break;
                 }
             }
             if is_boring==true {
                 boring+=1;
-                println!("{:?}", vec);
+                //println!("{:?}", vec);
             }
+
+            num_index+=1;
         }
-        println!("{}", boring);
+        println!("Case #{}: {}", i, boring);
     }
 }
