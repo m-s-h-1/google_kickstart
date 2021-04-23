c95d67a2ab59e52b6187b5ffd409183e25649653
Muhammad Saqib Hussain <saqib9217@live.com>
2021-04-23 20:42:02 +0500

    k-goodness string done

--- 2021/Round A/k-goodness string.rs
+++ 2021/Round A/k-goodness string.rs
@@ -1,38 +1,28 @@
-use std::io;
 fn main(){
     let mut t = String::new();
-    io::stdin().read_line(&mut t).expect("Error");
+    std::io::stdin().read_line(&mut t).expect("Error");
     let test_cases:u8 = t.trim().parse().expect("Error");
 
     //each test_case
     for i in 1..test_cases+1 {
         //setup
         let mut inp = String::new();
-        io::stdin().read_line(&mut inp).expect("Error");
+        std::io::stdin().read_line(&mut inp).expect("Error");
         let a:Vec<&str> = inp.split_whitespace().collect();
-        let mut length:usize = a[0].parse().unwrap();
-        length -=1;
-        let req_k:u32 = a[1].parse().unwrap();
+        let n:usize = a[0].parse().unwrap();
+        let req_k:i32 = a[1].parse().unwrap();
 
         //input string
-        let mut inp_string = String::new();
-        io::stdin().read_line(&mut inp_string).expect("Error");
-        let my_vec: Vec<char> = inp_string.chars().collect();
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).expect("Error");
 
-        
-
-        //calculate goodness
-        let mut score = 0;
-        let mut ans = 0;
-        for i in 0..length/2 {
-            println!("checking {} and {}", my_vec[i], my_vec[length-i]);
-            if my_vec[i] != my_vec[length-i] {
-                score+=1;
+        let mut cur_score = 0;
+        for i in 0..(n/2) {
+            if s.as_bytes()[i] != s.as_bytes()[n-1-i] {
+                cur_score += 1;
             }
         }
-        println!("Case #{}: {}", i, req_k-score);
+
+        println!("Case #{}: {}", i, ((cur_score-req_k) as i32).abs());
     }
 }
-
-//let first_letter = &inp_string[1..2];
-//println!("{}", first_letter);
--- a.cc
+++ a.cc
@@ -1,64 +1,20 @@
-    #include<bits/stdc++.h>
-    using namespace std;
-    #define LL long long
-    #define pa pair<int,int>
-    const int Maxn=610;
-    const LL inf=1e18;
-    const double pi=acos(-1.0);
-    LL read()
-    {
-    	LL x=0,f=1;char ch=getchar();
-    	while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}
-    	while(ch>='0'&&ch<='9')x=x*10ll+ch-'0',ch=getchar();
-    	return x*f;
-    }
-    int n,m,q,ans=0;
-    struct Edge
-    {
-    	int u,v;LL w;
-    }e[Maxn*Maxn];
-    LL dis[Maxn][Maxn];
-    vector<pa>h[Maxn];
-    LL mn[Maxn][Maxn];
-    int solve(int x,int y,int w)
-    {
-    	for(int i=1;i<=n;i++)
-    	if(dis[i][x]+w+mn[i][y]<=0)return 1;
-    	return 0;
-    }
-    int main()
-    {
-    	memset(mn,63,sizeof(mn));
-    	n=read(),m=read();
-    	for(int i=1;i<=n;i++)
-    	for(int j=1;j<=n;j++)
-    	dis[i][j]=inf;
-    	for(int i=1;i<=m;i++)
-    	{
-    		int x=e[i].u=read(),y=e[i].v=read();
-    		dis[x][y]=dis[y][x]=e[i].w=read();
-    	}
-    	for(int k=1;k<=n;k++)
-    	for(int i=1;i<=n;i++)
-    	for(int j=1;j<=n;j++)
-    	dis[i][j]=min(dis[i][j],dis[i][k]+dis[k][j]);
-    	for(int i=1;i<=n;i++)dis[i][i]=0;
-    	q=read();
-    	for(int i=1;i<=q;i++)
-    	{
-    		int u=read(),v=read(),l=read();
-    		h[u].push_back(make_pair(v,l));
-    		h[v].push_back(make_pair(u,l));
-    	}
-    	for(int i=1;i<=n;i++)
-    	for(int j=1;j<=n;j++)
-    	{
-    		for(int k=0;k<h[i].size();k++)
-    		{
-    			int p=h[i][k].first;LL l=h[i][k].second;
-    			mn[i][j]=min(mn[i][j],dis[p][j]-l);
-    		}
-    	}
-    	for(int i=1;i<=m;i++)ans+=(solve(e[i].u,e[i].v,e[i].w)|solve(e[i].v,e[i].u,e[i].w));
-    	printf("%d",ans);
-    }
+#include<bits/stdc++.h>
+
+int main() {
+	using namespace std;
+	ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+	int T; cin >> T;
+	for (int case_num = 1; case_num <= T; case_num ++) {
+		int N, K; cin >> N >> K;
+		string S; cin >> S;
+		int cur_score = 0;
+		for (int i = 0; i < N/2; i++) {
+			cur_score += (S[i] != S[N-1-i]);
+		}
+
+		cout << "Case #" << case_num << ": " << abs(cur_score - K) << '\n';
+	}
+
+	return 0;
+}
